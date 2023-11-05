d15f7e40a72505a0fc6e820342cc2f0a8fd8821e
Kaito Udagawa <umireon@gmail.com>
2023-11-06 01:53:23 +0900

    Update SampleHandler.swift

--- RecoreonBroadcastUploadExtension/SampleHandler.swift
+++ RecoreonBroadcastUploadExtension/SampleHandler.swift
@@ -9,11 +9,27 @@ import ReplayKit
 
 class SampleHandler: RPBroadcastSampleHandler {
     var matroska: Matroska?
+    
+    let dateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions.remove(.withDashSeparatorInDate)
+        formatter.formatOptions.remove(.withColonSeparatorInTime)
+        formatter.formatOptions.remove(.withTimeZone)
+        formatter.timeZone = TimeZone.current
+        return formatter
+    }()
+    
+    func generateFileName(date: Date, ext: String = "mkv") -> String {
+        let dateString = dateFormatter.string(from: date)
+        return "Recoreon\(dateString).\(ext)"
+    }
 
     override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
         matroska = Matroska()
-        let dir = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.github.umireon.Recoreon")
-        matroska?.open(dir?.appending(component: "aaa.mkv").path())
+        let dir = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.github.umireon.Recoreon")?.appendingPathComponent("Documents")
+        try? FileManager.default.createDirectory(at: dir!, withIntermediateDirectories: true)
+        let filename = generateFileName(date: Date())
+        matroska?.open(dir?.appending(component: filename).path())
     }
     
     override func broadcastPaused() {
@@ -25,7 +41,7 @@ class SampleHandler: RPBroadcastSampleHandler {
     }
     
     override func broadcastFinished() {
-//        matroska?.close()
+        matroska?.close()
     }
     
     override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
