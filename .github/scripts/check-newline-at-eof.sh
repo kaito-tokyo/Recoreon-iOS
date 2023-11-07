e7b912fc0ee355f62e92b1e6def5c63ea86f1fa0
Kaito Udagawa <umireon@gmail.com>
2023-11-08 05:18:05 +0900

    Check newline at eof (#3)
    * a
    
    * Update newline-at-eof.yaml
    
    * Fix names
    
    * Fix
    
    * Update newline-at-eof.yaml
    
    * Update newline-at-eof.yaml
    
    * Update check-newline-at-eof.sh
    
    * Fix
    
    * Fix

---
+++ .github/scripts/check-newline-at-eof.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+setopt EXTENDED_GLOB
+setopt PIPE_FAIL
+
+local -a files=(Recoreon*/**/*.(swift|m|h|plist|json)(.N))
+files+=(*.(xcodeproj|xcworkspace)/**/*(.N))
+files=(${files:#**/xcuserdata/**})
+files+=((.github|ci_scripts)/**/*(.N))
+files+=(*(.N))
+
+exit_status=0
+
+for file in ${(o)files}
+do
+  if [[ -z $(tail -c1 $file) ]]
+  then
+    printf 'OK: %s\n' "$file"
+  else
+    printf 'ERROR: %s\n' "$file"
+    exit_status=1
+  fi
+done
+
+exit $exit_status
