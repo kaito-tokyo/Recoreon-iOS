7bd8eaf43f496a8874e4ed6743c8f310f08db681
Kaito Udagawa <umireon@gmail.com>
2023-11-09 00:07:19 +0900

    Update PixelBufferExtractor.swift

--- RecoreonBroadcastUploadExtension/Services/PixelBufferExtractor.swift
+++ RecoreonBroadcastUploadExtension/Services/PixelBufferExtractor.swift
@@ -40,14 +40,27 @@ class PixelBufferExtractor {
       var attributes: [NSString: NSObject] = [:]
       attributes[kCVPixelBufferIOSurfacePropertiesKey] = [AnyHashable: Any]() as NSObject
       CVPixelBufferCreate(
-        kCFAllocatorDefault, width, height, kCVPixelFormatType_420YpCbCr8BiPlanarFullRange, attributes as CFDictionary?, &newPixelBufferRef)
+        kCFAllocatorDefault, width, height, kCVPixelFormatType_420YpCbCr8BiPlanarFullRange,
+        attributes as CFDictionary?, &newPixelBufferRef)
     }
     guard let newPixelBuffer = newPixelBufferRef else { return nil }
 
-    guard let srcLumaTexture = createTextureFromPixelBuffer(pixelBuffer, planeIndex: 0, format: .r8Unorm) else { return nil }
-    guard let srcChromaTexture = createTextureFromPixelBuffer(pixelBuffer, planeIndex: 1, format: .rg8Unorm) else { return nil }
-    guard let dstLumaTexture = createTextureFromPixelBuffer(newPixelBuffer, planeIndex: 0, format: .r8Unorm) else { return nil }
-    guard let dstChromaTexture = createTextureFromPixelBuffer(newPixelBuffer, planeIndex: 1, format: .rg8Unorm) else { return nil }
+    guard
+      let srcLumaTexture = createTextureFromPixelBuffer(
+        pixelBuffer, planeIndex: 0, format: .r8Unorm)
+    else { return nil }
+    guard
+      let srcChromaTexture = createTextureFromPixelBuffer(
+        pixelBuffer, planeIndex: 1, format: .rg8Unorm)
+    else { return nil }
+    guard
+      let dstLumaTexture = createTextureFromPixelBuffer(
+        newPixelBuffer, planeIndex: 0, format: .r8Unorm)
+    else { return nil }
+    guard
+      let dstChromaTexture = createTextureFromPixelBuffer(
+        newPixelBuffer, planeIndex: 1, format: .rg8Unorm)
+    else { return nil }
 
     guard let commandBuffer = commandQueue.makeCommandBuffer() else { return nil }
     guard let encoder = commandBuffer.makeBlitCommandEncoder() else { return nil }
@@ -60,12 +73,16 @@ class PixelBufferExtractor {
     return newPixelBuffer
   }
 
-  func createTextureFromPixelBuffer(_ pixelBuffer: CVPixelBuffer, planeIndex: Int, format: MTLPixelFormat) -> MTLTexture? {
+  func createTextureFromPixelBuffer(
+    _ pixelBuffer: CVPixelBuffer, planeIndex: Int, format: MTLPixelFormat
+  ) -> MTLTexture? {
     let width = CVPixelBufferGetWidthOfPlane(pixelBuffer, planeIndex)
     let height = CVPixelBufferGetHeightOfPlane(pixelBuffer, planeIndex)
 
     var textureRef: CVMetalTexture?
-    let ret = CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault, textureCache, pixelBuffer, nil, format, width, height, planeIndex, &textureRef)
+    let ret = CVMetalTextureCacheCreateTextureFromImage(
+      kCFAllocatorDefault, textureCache, pixelBuffer, nil, format, width, height, planeIndex,
+      &textureRef)
     if ret == kCVReturnSuccess {
       if let texture = textureRef {
         return CVMetalTextureGetTexture(texture)
