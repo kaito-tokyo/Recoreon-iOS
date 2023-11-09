db98100319db25d55a6b1301ea0aebb09024e2c6
Kaito Udagawa <umireon@gmail.com>
2023-11-10 02:08:06 +0900

    Tabview (#22)
    * Add RecordedVideoBasicView
    
    * Update ContentView.swift
    
    * Add
    
    * Update ContentView.swift

---
+++ Recoreon/RecordedVideoAdvancedView.swift
@@ -0,0 +1,12 @@
+import ReplayKit
+import SwiftUI
+
+struct RecordedVideoAdvancedView: View {
+  var body: some View {
+    Text("Coming soon...")
+  }
+}
+
+#Preview {
+  RecordedVideoAdvancedView()
+}
