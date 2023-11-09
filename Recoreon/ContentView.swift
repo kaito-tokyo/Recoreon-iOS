7e8b96eaa25fd37eca8dc7980f680fd8b428b073
Kaito Udagawa <umireon@gmail.com>
2023-11-10 01:55:01 +0900

    Update ContentView.swift

--- Recoreon/ContentView.swift
+++ Recoreon/ContentView.swift
@@ -5,7 +5,14 @@ struct ContentView: View {
   let recordedVideoManipulator: RecordedVideoManipulatorProtocol
 
   var body: some View {
-    RecordedVideoBasicView(recordedVideoManipulator: recordedVideoManipulator)
+    TabView {
+      Text("Record")
+        .tabItem { Image(systemName: "record.circle") }
+      RecordedVideoBasicView(recordedVideoManipulator: recordedVideoManipulator)
+        .tabItem { Image(systemName: "rectangle.grid.3x2" ) }
+      Text("Advenced View")
+        .tabItem { Image(systemName: "list.bullet") }
+    }
   }
 }
 
