5ed5227ecbdeb4c92c6631a75d2811c8517d2d25
Kaito Udagawa <umireon@gmail.com>
2023-11-13 00:11:33 +0900

    a

--- RecoreonTests/InputAudioFrameTests.m
+++ RecoreonTests/InputAudioFrameTests.m
@@ -13,11 +13,51 @@
 - (void)tearDown {
 }
 
-- (void)testInitWithSampleBuffer {
-  // InputAudioFrame *inputFrame = [[InputAudioFrame alloc]
-  // initWithSampleBuffer:nil sampleRate:44100]; XCTAssertNotNil(inputFrame);
-}
+- (CMSampleBufferRef)createInt16AudioSampleBuffer:(uint8_t *)buf bufSize:(uint32_t)bufSize numChannels:(int)numChannels sampleRate:(double)sampleRate isBigEndian:(BOOL)isBigEndian {
+  int numSamples = bufSize / numChannels;
 
+  AudioStreamBasicDescription asbd = {
+    .mSampleRate = sampleRate,
+    .mFormatID = kAudioFormatLinearPCM,
+    .mFormatFlags = kAudioFormatFlagIsBigEndian | kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked,
+    .mBytesPerPacket = 4,
+    .mFramesPerPacket = 1,
+    .mBytesPerFrame = 4,
+    .mChannelsPerFrame = 2,
+    .mBitsPerChannel = 16
+  };
+//  if (isBigEndian) {
+//    asbd.mFormatFlags |= kAudioFormatFlagIsBigEndian;
+//  }
+
+  CMFormatDescriptionRef format;
+  XCTAssertEqual(CMAudioFormatDescriptionCreate(NULL, &asbd, 0, NULL, 0, NULL, NULL, &format), noErr);
+
+  CMSampleTimingInfo timing = {
+    .duration = CMTimeMake(1, sampleRate),
+    .presentationTimeStamp = CMTimeMake(0, 1),
+    .decodeTimeStamp = CMTimeMake(0, 1)
+  };
+
+  CMSampleBufferRef sampleBuffer;
+  XCTAssertEqual(CMSampleBufferCreate(NULL, NULL, false, NULL, NULL, format, numSamples, 1, &timing, 0, NULL, &sampleBuffer), noErr);
+
+  AudioBufferList audioBufferList;
+  audioBufferList.mNumberBuffers = 1;
+  audioBufferList.mBuffers[0].mData = buf;
+  audioBufferList.mBuffers[0].mDataByteSize = 4096;
+  audioBufferList.mBuffers[0].mNumberChannels = 2;
+
+  XCTAssertEqual(CMSampleBufferSetDataBufferFromAudioBufferList(sampleBuffer, NULL, NULL, 0, &audioBufferList), noErr);
+
+  return sampleBuffer;
+}
+- (void)testInitWithSampleBuffer {
+  uint8_t buf[4096];
+  CMSampleBufferRef sampleBuffer = [self createInt16AudioSampleBuffer:buf bufSize:4096 numChannels:2 sampleRate:44100 isBigEndian:true];
+  InputAudioFrame *inputFrame = [[InputAudioFrame alloc] initWithSampleBuffer:sampleBuffer sampleRate:44100];
+  XCTAssertNotNil(inputFrame);
+}
 - (void)testPerformanceExample {
   // This is an example of a performance test case.
   [self measureBlock:^{
