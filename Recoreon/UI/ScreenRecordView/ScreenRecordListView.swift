41391002de8502867ae36d90cfa1c266083c6b7b
Kaito Udagawa <umireon@gmail.com>
2024-03-07 04:09:22 +0900

    Update ScreenRecordListView.swift

--- Recoreon/UI/ScreenRecordView/ScreenRecordListView.swift
+++ Recoreon/UI/ScreenRecordView/ScreenRecordListView.swift
@@ -123,7 +123,7 @@ struct ScreenRecordListView: View {
           }
         } label: {
           HStack {
-            if editMode.isEditing == true {
+            if editMode.isEditing {
               if selectedScreenRecordEntries.contains(screenRecordEntry) {
                 Image(systemName: "checkmark.circle")
                   .foregroundColor(.green)
