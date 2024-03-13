10978a41f25cb4cb61a2c169556ca0b109895e99
Kaito Udagawa <umireon@gmail.com>
2024-03-14 03:21:48 +0900

    Update ci_post_xcodebuild.sh

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,38 +1,38 @@
 #!/bin/zsh
 
-ls $CI_RESULT_BUNDLE_PATH
+setopt extendedglob
 
-# if [[ -d $CI_RESULT_BUNDLE_PATH ]];
-# then
-#   python3 -m venv .venv
+profdata=($CI_DERIVED_DATA_PATH/**/*.profdata(#qN))
 
-#   # shellcheck source=/dev/null
-#   . .venv/bin/activate
+if [[ -n $profdata ]];
+then
+  python3 -m venv .venv
 
-#   pip3 install codecov-cli
+  # shellcheck source=/dev/null
+  . .venv/bin/activate
 
-#   dest=($CI_DERIVED_DATA_PATH/**/Recoreon.app/Recoreon)
-#   profdata=($CI_DERIVED_DATA_PATH/**/*.profdata)
+  pip3 install codecov-cli
 
-#   case $CI_TEST_PLAN in
-#     RecoreonTests)
-#       xcrun llvm-cov show \
-#         -instr-profile "${profdata[1]}" \
-#         -ignore-filename-regex=".*/Recoreon/UI/.*" \
-#         "${dest[1]}" \
-#         >RecoreonTests.coverage.txt
+  dest=($CI_DERIVED_DATA_PATH/**/Recoreon.app/Recoreon)
 
-#       codecovcli upload-process --plugin "" --file RecoreonTests.coverage.txt
-#       ;;
+  case $CI_TEST_PLAN in
+    RecoreonTests)
+      xcrun llvm-cov show \
+        -instr-profile "${profdata[1]}" \
+        -ignore-filename-regex=".*/Recoreon/UI/.*" \
+        "${dest[1]}" \
+        >RecoreonTests.coverage.txt
+
+      codecovcli upload-process --plugin "" --file RecoreonTests.coverage.txt
+      ;;
     
-#     RecoreonUITests)
-#       xcrun llvm-cov show \
-#         -instr-profile "${profdata[1]}" \
-#         -ignore-filename-regex=".*/Recoreon/(Models|Services|Stores)/.*" \
-#         "${dest[1]}" \
-#         >RecoreonUITests.coverage.txt
+    RecoreonUITests)
+      xcrun llvm-cov show \
+        -instr-profile "${profdata[1]}" \
+        -ignore-filename-regex=".*/Recoreon/(Models|Services|Stores)/.*" \
+        "${dest[1]}" \
+        >RecoreonUITests.coverage.txt
 
-#       codecovcli upload-process --plugin "" --file RecoreonUITests.coverage.txt
-#   esac
-
-# fi
+      codecovcli upload-process --plugin "" --file RecoreonUITests.coverage.txt
+  esac
+fi
