f8b708e52eadccf6afde425429135639dfdd098d
Kaito Udagawa <umireon@gmail.com>
2024-07-20 02:23:24 +0900

    Update FragmentedScreenRecordWriter-Bridging-Header.h

--- FragmentedScreenRecordWriter/FragmentedScreenRecordWriter-Bridging-Header.h
+++ FragmentedScreenRecordWriter/FragmentedScreenRecordWriter-Bridging-Header.h
@@ -14,33 +14,51 @@ static inline void copyStereoInt16(float *__nonnull dst, int16_t *__nonnull src,
 static inline void copyStereoInt16UpsamplingBy2(float *__nonnull dst,
                                                 int16_t *__nonnull src,
                                                 long numSamples) {
-  for (long i = 0; i < numSamples; i++) {
+  {
+    float x0 = dst[-12];
+    float y0 = dst[-11];
+    float x1 = src[0] * INT16_TO_FLOAT_FACTOR;
+    float y1 = src[1] * INT16_TO_FLOAT_FACTOR;
+    for (long j = 1; j < 6; j++) {
+      dst[j * 2 - 12] = x0 + (x1 - x0) * j / 6.0;
+      dst[j * 2 - 11] = y0 + (y1 - y0) * j / 6.0;
+    }
+  }
+
+  for (long i = 0; i < numSamples - 1; i++) {
     float x0 = src[i * 2 + 0] * INT16_TO_FLOAT_FACTOR;
     float y0 = src[i * 2 + 1] * INT16_TO_FLOAT_FACTOR;
-    dst[i * 4 + 0] = x0;
-    dst[i * 4 + 1] = y0;
-    dst[i * 4 + 2] = x0;
-    dst[i * 4 + 3] = y0;
+    float x1 = src[i * 2 + 2] * INT16_TO_FLOAT_FACTOR;
+    float y1 = src[i * 2 + 3] * INT16_TO_FLOAT_FACTOR;
+    for (long j = 0; j < 2; j++) {
+      dst[i * 4 + j * 2 + 0] = x0 + (x1 - x0) * j / 2.0;
+      dst[i * 4 + j * 2 + 1] = y0 + (y1 - y0) * j / 2.0;
+    }
   }
 }
 
 static inline void copyStereoInt16UpsamplingBy6(float *__nonnull dst,
                                                 int16_t *__nonnull src,
                                                 long numSamples) {
-  for (long i = 0; i < numSamples; i++) {
+  {
+    float x0 = dst[-12];
+    float y0 = dst[-11];
+    float x1 = src[0] * INT16_TO_FLOAT_FACTOR;
+    float y1 = src[1] * INT16_TO_FLOAT_FACTOR;
+    for (long j = 1; j < 6; j++) {
+      dst[j * 2 - 12] = x0 + (x1 - x0) * j / 6.0;
+      dst[j * 2 - 11] = y0 + (y1 - y0) * j / 6.0;
+    }
+  }
+
+  for (long i = 0; i < numSamples - 1; i++) {
     float x0 = src[i * 2 + 0] * INT16_TO_FLOAT_FACTOR;
     float y0 = src[i * 2 + 1] * INT16_TO_FLOAT_FACTOR;
-    dst[i * 12 + 0] = x0;
-    dst[i * 12 + 1] = y0;
-    dst[i * 12 + 2] = x0;
-    dst[i * 12 + 3] = y0;
-    dst[i * 12 + 4] = x0;
-    dst[i * 12 + 5] = y0;
-    dst[i * 12 + 6] = x0;
-    dst[i * 12 + 7] = y0;
-    dst[i * 12 + 8] = x0;
-    dst[i * 12 + 9] = y0;
-    dst[i * 12 + 10] = x0;
-    dst[i * 12 + 11] = y0;
+    float x1 = src[i * 2 + 2] * INT16_TO_FLOAT_FACTOR;
+    float y1 = src[i * 2 + 3] * INT16_TO_FLOAT_FACTOR;
+    for (long j = 0; j < 6; j++) {
+      dst[i * 12 + j * 2 + 0] = x0 + (x1 - x0) * j / 6.0;
+      dst[i * 12 + j * 2 + 1] = y0 + (y1 - y0) * j / 6.0;
+    }
   }
 }
